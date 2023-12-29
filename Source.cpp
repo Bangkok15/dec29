a046748223153deb62a756bfa17ce76c99aef8e2
Bangkok15 <kazya07@yandex.ru>
2023-12-29 20:16:11 +0300

    Добавил пару строчек

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
 
+using namespace std;
 
 
 int main() {
-	
+	setlocale(LC_ALL, "RUS");
+	int a, b, c;
+	cin >> a;
+	cin >> b;
+	c = a + b;
+	cout << "Сумма чисел = " << c;
 	
 	return 0;
 }
